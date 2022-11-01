597b74412ef785f579c09ba4e08d7ed73c4e0cb0
Divya-1905 <divyasris@venzotechnologies.com>
2022-11-01 16:48:24 +0530

    proc

--- core/routing.py
+++ core/routing.py
@@ -4,5 +4,5 @@ from channels.routing import ProtocolTypeRouter,URLRouter
 from django.urls import path
 from .consumer import ChatConsumer
 websocket_urlspatterns = [
-    path('ws/asgi/<str:room_name>/',ChatConsumer.as_asgi()),
+    path('ws/<str:room_name>/',ChatConsumer.as_asgi()),
 ]
